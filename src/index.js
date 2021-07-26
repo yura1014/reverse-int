5e9ce5ca1fbaaa88b4cef00d60347c48e70013a4
yura1014 <62890040+yura1014@users.noreply.github.com>
2021-07-26 11:19:45 +0300

    fetch: start reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let arr;
+  arr = String(n).split("");
+  if (n > 0) {
+    return Number(arr.reverse().join(""));
+  } else {
+    arr.splice(0, 1);
+    arr.reverse();
+    arr.splice(0, 0, "-");
+    return Number(arr.join(""));
+  }
 }
